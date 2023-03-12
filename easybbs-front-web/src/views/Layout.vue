40278d515bb1fda0f89da477bf45934a23a3703b
lxw <1816202461@qq.com>
2023-03-12 17:14:56 +0800

    测试

---
+++ easybbs-front-web/src/views/Layout.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="header">
+
+    </div>
+
+    <div>
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+</style>
